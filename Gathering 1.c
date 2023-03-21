a8cdef8911a6f128976a9fb13a6f2bf347941443
purvakhant421 <115995434+purvakhant421@users.noreply.github.com>
2023-03-21 23:00:34 +0530

    Add files via upload

---
+++ Gathering 1.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int fact(int a){
+    if(a<=1){
+        return 1;
+    }
+    else{
+        return a*fact(a-1);
+    }
+}
+int main(){
+    int y;
+    printf("Enter a Number:");
+    scanf("%d",&y);
+    
+    printf("factorial of %d",fact(y));
+
+  return 0;
+}
